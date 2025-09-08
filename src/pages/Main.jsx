71f9bfaf2cadb857d244b55d821c188460ee57ab
Eirik Hafver Rønjum <64041578+eirikconteir@users.noreply.github.com>
2025-09-08 10:33:04 +0200

    Fjernet advarsler. Byggbar versjon

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,9 +1,9 @@
 import React from "react"
 import "bootstrap/dist/css/bootstrap.min.css"
 import "../index.css"
-import { MaalAutosuggest } from "../components/MaalAutosuggest"
-import { DiagnoseAutosuggest } from "../components/DiagnoseAutosuggest"
-import { IntervjensjonAutosuggest } from "../components/IntervjensjonAutosuggest"
+//import { MaalAutosuggest } from "../components/MaalAutosuggest"
+//import { DiagnoseAutosuggest } from "../components/DiagnoseAutosuggest"
+//import { IntervjensjonAutosuggest } from "../components/IntervjensjonAutosuggest"
 import { Footer } from "../components/footer"
 import { Header } from "../components/Header"
 
